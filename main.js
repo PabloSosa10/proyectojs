8f2fad1b2515d7d084efcd84ee55ceb87339ff9e
Pablo Sosa <pabloagustinsosa19@gmail.com>
2025-05-15 14:33:22 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let cliente;
+
+cliente = "carlos";
+
+clientes = "samuel";
+console.log(cliente);
+
+
+let edad = 25; 
+
+console.log(edad);
+
+//constantes
+const nacimento = 1998;
+console.log(nacimento);
+
